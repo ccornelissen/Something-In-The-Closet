07e67f930b01a9c8c49f39605e03ffe37cc3342a
CODYASUS\Cody <codycornelissen@gmail.com>
2017-05-22 18:01:37 -0700

    Base project comit.

---
+++ UE4_Project/Source/UE4_Project/UE4_ProjectGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4_Project.h"
+#include "UE4_ProjectGameModeBase.h"
+
+
+
+
---
+++ UE4_Project/Source/UE4_Project/UE4_ProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_PROJECT_API AUE4_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
